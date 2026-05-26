5f627c4efa3d665b4f680d0a00d17fd0408631c3
Sayad77 <votre_email_pour_sayadali.madarbukus@gmail.com>
2026-05-26 12:20:21 +0200

    intégration du script de suivi Google Analytics GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
-import dynamic from "next/dynamic"; // Nécessaire pour utiliser dynamic()
-import { PlayerProvider } from "@/app/context/PlayerContext"; // Vérifie que ton dossier est bien "context" à la racine ou dans src
+import dynamic from "next/dynamic";
+import { PlayerProvider } from "@/app/context/PlayerContext";
 import Preloader from "@/app/components/PreLoader";
 import CookieConsent from "@/app/components/CookieConsent";
+import Script from "next/script"; // <-- Importation indispensable pour Google Analytics
 import "./globals.css";
 import PlayerBarClient from "./components/PlayerBarClients";
+
 const playfair = Playfair_Display({
   subsets: ["latin"],
   variable: "--font-serif",
@@ -31,6 +33,23 @@ export default function RootLayout({
   return (
     <html lang="fr" className={`${playfair.variable} ${inter.variable}`}>
       <body className="antialiased bg-black text-neutral-200 font-sans selection:bg-white selection:text-black">
+        
+        {/* --- DÉBUT : GOOGLE ANALYTICS --- */}
+        {/* N'oublie pas de remplacer G-XXXXXXXXXX par ton vrai ID de mesure */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-RSPDVNVMPG`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RSPDVNVMPG');
+          `}
+        </Script>
+        {/* --- FIN : GOOGLE ANALYTICS --- */}
+
         <PlayerProvider>
           {/* Preloader */}
           <Preloader />
